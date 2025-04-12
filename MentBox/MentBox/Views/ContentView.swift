116a1471c75fe2e5b133c88dc380f714383563b4
bishoe01 <bishoe01@gmail.com>
2025-04-12 23:47:30 +0900

    design : Geologica, pretendard 폰트 추가

--- MentBox/MentBox/Views/ContentView.swift
+++ MentBox/MentBox/Views/ContentView.swift
@@ -1,22 +1,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    let firestoreService = FirestoreService()
+//    let firestoreService = FirestoreService()
 
     var body: some View {
         VStack {
             Text("MentBox 연결 테스트 완료!")
+            Text("MentBox 연결 테스트 완료!")
                 .padding()
+                .font(.custom("PretendardVariable-Regular", size: 24))
+            Text("MentBox 연결 테스트 완료!")
+                .padding()
+                .font(.custom("Geologica-Bold", size: 24))
         }
-        .onAppear {
-            firestoreService.addTestDocument { error in
-                if let error = error {
-                    print("❌ Firestore 저장 실패: \(error)")
-                } else {
-                    print("✅ Firestore 저장 성공!")
-                }
-            }
-        }
+//        .onAppear {
+//            firestoreService.addTestDocument { error in
+//                if let error = error {
+//                    print("❌ Firestore 저장 실패: \(error)")
+//                } else {
+//                    print("✅ Firestore 저장 성공!")
+//                }
+//            }
+//        }
     }
 }
 
