ce6a8f0cb07d3eb656a10ff8eaddfd244b880304
bishoe01 <bishoe01@gmail.com>
2025-05-03 11:42:09 +0900

    fix : Group 제네릭 추론 오류 해결 - ViewBuilder 루트 뷰 분기 처리

--- MentBox/MentBox/Managers/NavigationManager.swift
+++ MentBox/MentBox/Managers/NavigationManager.swift
@@ -1,36 +1,66 @@
 import SwiftUI
 
-enum AuthViewType: Hashable {
+// Auth뷰
+enum AuthView: Hashable {
+    case login
     case userTypeSelection
     case userInfoInput(UserType)
 }
 
-enum MainViewType: Hashable {
-    case mentor
-    case learner
+// Mentor뷰
+enum MentorView: Hashable {
+    case profile
+    case home
+    case myPage
 }
 
-class NavigationManager<T: Hashable>: ObservableObject {
-    @Published var path = NavigationPath()
+// Learner뷰
+enum LearnerView: Hashable {
+    case home
+    case chatRoom(mentorId: String)
+    case profile
+    case myPage
+    case bookMark
+}
 
-    func navigate(to destination: T) {
+class NavigationManager: ObservableObject {
+    @Published var path = NavigationPath()
+    @Published var rootView: AuthView = .login
+    @Published var mentorView: MentorView?
+    @Published var learnerView: LearnerView?
+    
+    func navigate(to destination: MentorView) {
+        path.append(destination)
+    }
+    
+    func navigate(to destination: LearnerView) {
         path.append(destination)
     }
 
     func popToRoot() {
-        path.removeLast(path.count)
+        path = NavigationPath()
     }
 
     func pop() {
         path.removeLast()
     }
+    
+    func setMainRoot(userType: UserType) {  //로그인할 떄 넣어줘야하는 페이지 
+        path = NavigationPath()
+        rootView = .userTypeSelection
+        if userType == .mentor { // 멘토뷰 
+            mentorView = .profile
+            learnerView = nil
+        } else {  //러너 뷰 
+            mentorView = nil
+            learnerView = .profile
+        }
+    }
+    
+    func setAuthRoot() {  // 로그아웃 했을 때 씀 
+        path = NavigationPath()
+        rootView = .login
+        mentorView = nil
+        learnerView = nil
+    }
 }
-
-// MARK: 네비게이션 매니저 분리
-
-// 인증 관련 네비게이션  -> 회원가입 까지만
-// 메인 네비게이션 -> 로그인 이후
-
-let authNavigationManager = NavigationManager<AuthViewType>()
-
-let mainNavigationManager = NavigationManager<MainViewType>()
--- MentBox/MentBox/MentBoxApp.swift
+++ MentBox/MentBox/MentBoxApp.swift
@@ -14,23 +14,47 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 @main
 struct MentBoxApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    @StateObject var authNavigationManager = AuthNavigationManager()
+    @StateObject private var navigationManager = NavigationManager()
 
-
+    // 다크모드 강제 (Scene이 여러 개일 때는 추가 처리 필요)
     init() {
-        // 다크 모드 강제 설정
-        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
-            if let window = windowScene.windows.first {
-                window.overrideUserInterfaceStyle = .dark
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let window = windowScene.windows.first
+        {
+            window.overrideUserInterfaceStyle = .dark
+        }
+    }
+
+    // Group 'v'에러 -> ViewBuilder로 조건부 뷰 보여주기
+    @ViewBuilder
+    private var rootContent: some View {
+        if let mentorView = navigationManager.mentorView {
+            MentorMainView()
+        } else if let learnerView = navigationManager.learnerView {
+            LearnerMainView()
+        } else {
+            switch navigationManager.rootView {
+            case .login:
+                SignInView()
+            case .userTypeSelection:
+                UserTypeSelectionView(
+                    selectedUserType: .constant(nil)
+                ) { userType in
+                    navigationManager.rootView = .userInfoInput(userType)
+                }
+            case .userInfoInput(let userType):
+                UserInfoInputView(userType: userType) {
+                    navigationManager.setMainRoot(userType: userType)
+                }
             }
         }
     }
-    
+
     var body: some Scene {
         WindowGroup {
-            SignInView()
-                .ignoresSafeArea(.all)
-                .environmentObject(authNavigationManager)
+            rootContent // 조건 분기는 모두 여기서 해결
+                .ignoresSafeArea()
+                .environmentObject(navigationManager)
         }
     }
 }
